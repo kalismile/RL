f7fc3ce4b1e94c9abf9375fd70c707ff2f9ccd51
kalismile <kalismile@outlook.com>
2019-10-15 00:23:35 +0800

    initial

---
+++ test1/a1.py
@@ -0,0 +1,43 @@
+from cv2 import cv2 as cv
+import numpy
+import time
+import os
+
+# image1=cv.imread('C:\\Users\\kali\\Desktop\\1.jpg',cv.IMREAD_UNCHANGED)
+# image2=cv.imread('C:\\Users\\kali\\Desktop\\2.jpg',cv.IMREAD_UNCHANGED)
+# cv.namedWindow('image',cv.WINDOW_AUTOSIZE)
+# image=cv.add(image1,image2)
+# cv.imshow("image",image)
+
+# cv.waitKey(5000)
+
+# cv.destroyAllWindows()
+
+# cap=cv.VideoCapture('C:\\Users\\kali\\Desktop\\1.mp4')
+# while cap.isOpened():
+#     ret,frame=cap.read()
+#     if not ret:
+#         break
+#     print(frame.shape)
+#     print(frame.size)
+#     print(frame.dtype)
+#     cv.imshow('mp4',frame)
+#     cv.waitKey(25)
+
+os.system('ffplay C:\\Users\\kali\\Desktop\\1.mp4')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(image.shape)
+# print(image.size )
+# print(image.dtype )
