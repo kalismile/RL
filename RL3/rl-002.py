0921ae95ceb4419a2b0d29ed81e999031cb9424d
kalismile <kalismile@outlook.com>
2019-11-26 21:40:20 +0800

    3

--- RL3/rl-002.py
+++ RL3/rl-002.py
@@ -2,21 +2,25 @@
 import tensorflow as tf  
 import numpy as np  
 import gym
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+tf.test.gpu_device_name()
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+print('GPU is there',tf.test.is_gpu_available())
 
-
-
-env=gym.make('CartPole-v0')
-env.reset()
-print(env.metadata)
-for i in range(1000):
-    env.step(env.action_space.sample())
-    env.render()
-env.close()
+# env=gym.make('CartPole-v0')
+# env.reset()
+# print(env.metadata)
+# for i in range(1000):
+#     env.step(env.action_space.sample())
+#     env.render()
+# env.close()
 
 a1=np.arange(1,10).reshape(3,3)
 a2=np.arange(10,19).reshape(3,3)
 b1=tf.constant(a1)
 b2=tf.constant(a2)
-for i in range(1000):
-    print(tf.matmul(b1,b2))
+for i in range(1):
+    e=tf.matmul(b1,b2)
 
